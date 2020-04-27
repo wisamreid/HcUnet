0ee49b1420d73a698c6b6408e0fd7733405f7d60
buswinka <buswinka@gmail.com>
2020-04-27 15:12:08 -0400

    cleaned up predict_mask by adding another function: calculate inexes in utils

--- utils.py
+++ utils.py
@@ -45,7 +45,7 @@ def pad_image_with_reflections(image, pad_size=(30, 30, 6)):
     return out
 
 
-def predict_mask(model, image):
+def predict_mask(model, image, device):
     """
     Takes in a model and an image and applies the model to all parts of the image.
 
@@ -62,90 +62,59 @@ def predict_mask(model, image):
     :return mask:
 
     """
-    EVAL_IMAGE_SIZE = (200, 200, 12)
-    PAD_SIZE = (100, 100, 6)
+    EVAL_IMAGE_SIZE = (250, 250, 12)
+    PAD_SIZE = (100, 100, 10)
 
-    if torch.cuda.is_available():
-        device = 'cuda:0'
-    else:
-        device = 'cpu'
-
-    model.to(device)
-    model.eval()
-
-    mask = torch.zeros(image.shape).to(device)
+    mask = torch.zeros(image.shape)
 
     # Apply Padding
     padded_image = pad_image_with_reflections(image, pad_size=PAD_SIZE)
 
-    print(padded_image.shape)
 
     #  We now calculate the indicies for our image
-    # X
-    xlist = torch.arange(PAD_SIZE[0], image.shape[2], EVAL_IMAGE_SIZE[0])
-    x_ind = []
-    for i, x in enumerate(xlist):
-        if i == 0:
-            continue
-        x1 = int(xlist[i-1])-PAD_SIZE[0]
-        x2 = int(x-1)+PAD_SIZE[0]
-        x_ind.append([x1, x2])
-    x1 = padded_image.shape[2]-(EVAL_IMAGE_SIZE[0] + PAD_SIZE[0]*2)
-    x2 = padded_image.shape[2]-1
-    x_ind.append([x1, x2])
-    print(x_ind)
-
-    # Y
-    ylist = torch.arange(PAD_SIZE[1], image.shape[3], EVAL_IMAGE_SIZE[1])
-    y_ind = []
-    for i, y in enumerate(ylist):
-        if i == 0:
-            continue
-        y1 = int(ylist[i-1])-PAD_SIZE[1]
-        y2 = int(y-1)+PAD_SIZE[1]
-        y_ind.append([y1, y2])
-    y1 = padded_image.shape[3]-(EVAL_IMAGE_SIZE[1] + PAD_SIZE[1]*2)
-    y2 = padded_image.shape[3]-1
-    y_ind.append([y1, y2])
-    print(y_ind)
-
-    # Z
-    zlist = torch.arange(PAD_SIZE[2], image.shape[4], EVAL_IMAGE_SIZE[2])
-    z_ind = []
-    for i, z in enumerate(zlist):
-        if i == 0:
-            continue
-        z1 = int(zlist[i-1])-PAD_SIZE[2]
-        z2 = int(z-1)+PAD_SIZE[2]
-        z_ind.append([z1, z2])
-    if not z_ind:  # Sometimes z is so small the first part doesnt work. Check if z_ind is empty, if it is do this!!!
-        z1 = 0
-        z2 = EVAL_IMAGE_SIZE[2] + PAD_SIZE[2]
-        z_ind.append([z1, z2])
-        z_ind.append([padded_image.shape[4] - EVAL_IMAGE_SIZE[2], padded_image.shape[4]])
-    else:
-        z1 = padded_image.shape[4]-(EVAL_IMAGE_SIZE[2] + PAD_SIZE[2]*2)
-        z2 = padded_image.shape[4]-1
-        z_ind.append([z1, z2])
-    print(z_ind)
+    x_ind = calculate_indexes(PAD_SIZE[0], EVAL_IMAGE_SIZE[0], image.shape[2], padded_image.shape[2])
+    y_ind = calculate_indexes(PAD_SIZE[1], EVAL_IMAGE_SIZE[1], image.shape[3], padded_image.shape[3])
+    z_ind = calculate_indexes(PAD_SIZE[2], EVAL_IMAGE_SIZE[2], image.shape[4], padded_image.shape[4])
 
     # Loop and apply unet
     for i, x in enumerate(x_ind):
         for j, y in enumerate(y_ind):
             for k, z in enumerate(z_ind):
                 padded_image_slice = padded_image[:,:,x[0]:x[1], y[0]:y[1]:, z[0]:z[1]]
-                # out = model(padded_image_slice.float().to(device))
-                out = padded_image_slice[:, 1, 0:230, 0:230, 0:14]
+                print(x,y,z,padded_image_slice.shape)
+                with torch.no_grad():
+                    out = model(padded_image_slice.float().to(device))
+
                 valid_out  = out[:,:,
                              PAD_SIZE[0]:EVAL_IMAGE_SIZE[0]+PAD_SIZE[0],
                              PAD_SIZE[1]:EVAL_IMAGE_SIZE[1]+PAD_SIZE[1],
                              PAD_SIZE[2]:EVAL_IMAGE_SIZE[2]+PAD_SIZE[2],
                                  ]
 
-                mask[:, :, x[0]:x[0]+EVAL_IMAGE_SIZE[0]-1,
-                           y[0]:y[0]+EVAL_IMAGE_SIZE[1]-1,
-                           z[0]:z[0]+EVAL_IMAGE_SIZE[2]-1] = valid_out
+                mask[:, :, x[0]:x[0]+valid_out.shape[2],
+                           y[0]:y[0]+valid_out.shape[3],
+                           z[0]:z[0]+valid_out.shape[4]] = valid_out
+
+    return mask
 
 
+def calculate_indexes(pad_size, eval_image_size, image_shape, dim_shape):
+    ind_list = torch.arange(pad_size, image_shape, eval_image_size)
+    ind = []
+    for i, z in enumerate(ind_list):
+        if i == 0:
+            continue
+        z1 = int(ind_list[i-1])-pad_size
+        z2 = int(z-1)+pad_size
+        ind.append([z1, z2])
+    if not ind:  # Sometimes z is so small the first part doesnt work. Check if z_ind is empty, if it is do this!!!
+        z1 = 0
+        z2 = eval_image_size + pad_size * 2
+        ind.append([z1, z2])
+        ind.append([dim_shape - (eval_image_size+pad_size*2), dim_shape])
+    else:
+        z1 = dim_shape-(eval_image_size + pad_size*2)
+        z2 = dim_shape-1
+        ind.append([z1, z2])
+    return ind
 
-    return None
